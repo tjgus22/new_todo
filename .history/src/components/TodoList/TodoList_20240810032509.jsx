26bb03afcd0126690064be95171efaf86429e67e
tjgus22 <gkstjgus1992@gmail.com>
2024-08-12 18:17:55 +0900

    기본todo

---
+++ .history/src/components/TodoList/TodoList_20240810032509.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default function TodoList() {
+  const { todos, setTodos } = useState([
+    { id: 123, text: '장보기', status: 'active' },
+    { id: 456, text: '공부하기', status: 'active' },
+  ]);
+  return <div></div>;
+}
